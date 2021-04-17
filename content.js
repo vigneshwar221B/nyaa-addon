20d760738fc00bbbbb21c12feca3e7ae0d6686e0
vignesh war <vigneshwar@vigneshs-MacBook-Air.local>
2021-04-17 21:53:01 +0530

    UI: added the play button

--- content.js
+++ content.js
@@ -1,1 +1,17 @@
-console.log('nyaa addon');
+const addButtons = (rows) => {
+  const newLink = `<a href="#"><i class="fa fa-fw fa-play"></i></a>`;
+
+  rows.forEach((row) => {
+    const selectedRows = document.querySelectorAll(`tr.${row}`);
+    selectedRows.forEach((element) => {
+      element.cells[2].innerHTML = element.cells[2].innerHTML + newLink;
+    });
+  });
+
+  console.log("added new buttons");
+};
+
+document.addEventListener("DOMContentLoaded", function (event) {
+  console.log("nyaa addon");
+  addButtons(["default", "danger", "success"]);
+});
