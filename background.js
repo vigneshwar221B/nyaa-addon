e1e722984fb8aec3867674cdf747e537073b4b86
vignesh war <vigneshwar@vigneshs-MacBook-Air.local>
2021-04-17 23:24:56 +0530

    I'm getting somewhere

--- background.js
+++ background.js
@@ -1,1 +1,8 @@
 console.log("background: nyaa addon");
+
+const baseUrl = chrome.extension.getURL("html/index.html");
+console.log(baseUrl);
+
+chrome.storage.sync.set({ baseUrl }, function () {
+  console.log("Value is set");
+});
--- content.js
+++ content.js
@@ -1,9 +1,11 @@
 const addButtons = (rows) => {
-  const newLink = `<a href="#"><i class="fa fa-fw fa-play"></i></a>`;
-
+  let baseUrl = chrome.extension.getURL("html/index.html");
   rows.forEach((row) => {
     const selectedRows = document.querySelectorAll(`tr.${row}`);
+
     selectedRows.forEach((element) => {
+      const magnetUri = element.cells[2].children[1].href;
+      const newLink = `<a target="_blank" href="${baseUrl}&muri=${magnetUri}"><i class="fa fa-fw fa-play"></i></a>`;
       element.cells[2].innerHTML = element.cells[2].innerHTML + newLink;
     });
   });
@@ -11,7 +13,8 @@ const addButtons = (rows) => {
   console.log("added new buttons");
 };
 
-document.addEventListener("DOMContentLoaded", function (event) {
+document.addEventListener("DOMContentLoaded", (event) => {
   console.log("nyaa addon");
   addButtons(["default", "danger", "success"]);
+  console.log(chrome.extension.getURL("html/index.html"));
 });
